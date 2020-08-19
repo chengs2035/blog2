9e3c308ffff264eb341d9c28e72f155a50046496
chengs2035 <chensongguo@gmail.com>
2020-08-19 13:13:47 +0800

    https://www.djc8.cn/archives/spring-cloud-learning-notes-5-zuul-implementation-of-routing-gateway.html

---
+++ zuulserver/src/test/java/cn/djc8/zuulserver/ZuulserverApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.djc8.zuulserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
