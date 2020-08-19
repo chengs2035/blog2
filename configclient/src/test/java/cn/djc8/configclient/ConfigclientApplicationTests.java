dc59199d1f5099b03f2b7b5ab77a2796ba3e7d88
chengs2035 <chensongguo@gmail.com>
2020-08-19 21:35:12 +0800

    configserver3 https://www.djc8.cn/archives/spring-cloud-learning-notes-6-distributed-configuration-center.html

---
+++ configclient/src/test/java/cn/djc8/configclient/ConfigclientApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.djc8.configclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
